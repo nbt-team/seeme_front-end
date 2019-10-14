9015adce083061cbada267c16b758edca0ba0d17
AntonDeXy <porolyananton@gmail.com>
2019-10-14 14:36:15 +0300

    add menu on mobile

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,25 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import logo from '../static/logo.svg'
+import menu from '../static/menu.svg'
 
 const Header = () => {
+  const menuItems = useRef(null)
+  let isClicked = false
+  let menuItemsStyle
+  
+  const menuClick = () => {
+    isClicked ? isClicked = false : isClicked = true
+
+    isClicked
+    ? menuItemsStyle = { display: 'block' }
+    : menuItemsStyle = { display: 'none' }
+  }
   return (
     <header>
       <div className="wrapper">
-        <img src={logo} alt=""/>
-        <div className="menu">
+        <img src={logo} alt="" />
+        <img onClick={() => { menuClick() }} className='menuIcon' src={menu} alt="" />
+        <div ref={menuItems} style={menuItemsStyle} className="menu">
           <a href='/'>item</a>
         </div>
       </div>
