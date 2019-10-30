3f72e55c7ff7814a02640b790a9bd7f710763395
AntonDeXy <porolyananton@gmail.com>
2019-10-30 14:21:53 +0200

    input customize

--- src/components/General.jsx
+++ src/components/General.jsx
@@ -1,10 +1,20 @@
 import React, { useState, useRef } from 'react'
 import { Link } from "react-router-dom"
+// import classie from '../static/classie'
 
 const General = () => {
   const [hostLive, setHostLive] = useState(false)
+
+  const [inputEl1IsFocused, setInputEl1] = useState(false)
+  const [inputEl2IsFocused, setInputEl2] = useState(false)
+
   const hostLiveCB = useRef(null)
   const connectLiveCB = useRef(null)
+
+  const inputEl1 = useRef(null)
+  const inputEl2 = useRef(null)
+
+
   const checkForSelected = () => {
     if (hostLiveCB.current.checked) {
       setHostLive(true)
@@ -19,45 +29,70 @@ const General = () => {
   } else {
     style = {}
   }
+
   let chatId = '82378d238jdhd2'
+
+  const inputIsAvtive = () => {
+    console.log(inputEl1.current.value)
+    // console.log(inputEl1.current.onfocus)
+    // console.log(inputEl1.current.onblur)
+  }
+
+  if (inputEl1IsFocused) {
+    inputEl1.current.style.borderColor = '#da6484' 
+  }
+
+  if (inputEl2IsFocused) {
+    inputEl2.current.style.borderColor = '#da6484' 
+  }
 
   return (
     <main>
       <div className="wrapper">
         <h1>Welcome to</h1>
         <h1>See Me</h1>
-        <div className="checkboxes">
-          <div>
-
-            <label class="container">Host your live
-              <input onChange={checkForSelected} ref={hostLiveCB} type="radio" name="type" id="hostLive" />
-              <span class="checkmark"></span>
-            </label>
-          </div>
-          <div>
-            <label class="container">Connect to live
-              <input defaultChecked onChange={checkForSelected} ref={connectLiveCB} type="radio" name="type" id="connectLive" />
-              <span class="checkmark"></span>
-            </label>
-          </div>
+        <CheckBoxes checkForSelected={checkForSelected} hostLiveCB={hostLiveCB} connectLiveCB={connectLiveCB} />
+        <div onClick={inputIsAvtive} className="inputs">
+          { inputEl1IsFocused && <span>Your Nickname</span> }
+          <input onFocus={() => {setInputEl1(true)}} type="text" ref={inputEl1} className='inputInGeneralPage' placeholder='Your nickname' />
+          { inputEl2IsFocused && <span>Stream URL</span> }
+          <input onFocus={() => {setInputEl2(true)}} id='url' ref={inputEl2} className='inputInGeneralPage' type="text" value={hostLive ? chatId : ''} placeholder='Enter stream URL' />
         </div>
-        <div className="inputs">
-          <input type="text" placeholder='Your nickname' />
-          <input id='url' type="text" value={hostLive ? chatId : ''} placeholder='Enter stream URL' />
-        </div>
-        <div className="buttons">
-          {hostLive &&
-            <Link to='/chat'>
-              <button class="button button--aylen button--border-thick button--inverted button--text-upper button--size-s">Start chat</button>
-            </Link>
-          }
-          <Link to={`/chat/${chatId}`} style={style}>
-            <button class="button button--aylen button--border-thick button--inverted button--text-upper button--size-s">Connect</button>
-          </Link>
-        </div>
+        <Buttons hostLive={hostLive} chatId={chatId} style={style} />
       </div>
+
     </main>
   )
 }
+
+const CheckBoxes = ({ checkForSelected, hostLiveCB, connectLiveCB }) => (
+  <div className="checkboxes">
+    <div>
+      <label class="container">Host your live
+        <input onChange={checkForSelected} ref={hostLiveCB} type="radio" name="type" id="hostLive" />
+        <span class="checkmark"></span>
+      </label>
+    </div>
+    <div>
+      <label class="container">Connect to live
+        <input defaultChecked onChange={checkForSelected} ref={connectLiveCB} type="radio" name="type" id="connectLive" />
+        <span class="checkmark"></span>
+      </label>
+    </div>
+  </div>
+)
+
+const Buttons = ({ hostLive, chatId, style }) => (
+  <div className="buttons">
+    {hostLive &&
+      <Link to='/chat'>
+        <button class="button button--aylen button--border-thick button--inverted button--text-upper button--size-s">Start chat</button>
+      </Link>
+    }
+    <Link to={`/chat/${chatId}`} style={style}>
+      <button class="button button--aylen button--border-thick button--inverted button--text-upper button--size-s">Connect</button>
+    </Link>
+  </div>
+)
 
 export default General
